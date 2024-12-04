4f4e8bb61c146598c8fc40d762073e9467ede2dc
hsmith56 <47962432+hsmith56@users.noreply.github.com>
2024-12-03 19:56:56 -0500

    day 3, big L.

--- 2024/day03/day03.py
+++ 2024/day03/day03.py
@@ -1,16 +1,59 @@
 import random, math
+import re
 
-def load():
-	with open('day03/day03Input.txt') as file:
-		data = file.readlines()
-		d = [line.strip() for line in data]
-	return d
+def load(example=False):
+    if example is True:
+        with open('day03Example.txt') as file:
+            data = file.readlines()
+            d = [line.strip() for line in data]
+        return d
+        
+    with open('day03Input.txt') as file:
+        data = file.readlines()
+        d = [line.strip() for line in data]
+    return d
 
-inpt = load()
+inpt = load(example=False)
+
 def part1(inpt):
-	return inpt
+    pattern = r"mul\(\d*,\d*\)"
+
+    running_total = 0
+
+    for line in inpt:
+        matches = re.findall(pattern, line)
+        for match in matches:
+            match = match.replace("mul(","").replace(")","").split(",")
+
+            match = [int(x) for x in match]
+            running_total += match[0] * match[1]
+
+    return running_total
 
 def part2(inpt):
-	return 0
+    pattern = r"mul\(\d*,\d*\)"
 
-print(f'Part1: {part1(inpt)}\nPart2: {part2(inpt)}')
+    running_total = 0
+
+    inpt = "".join(inpt)
+    matches = re.finditer(pattern, inpt)
+    for match in matches:
+
+        search_upto_index = match.span()[0]
+
+        do_instruction   =  max(inpt[0:search_upto_index+1].rfind('do()'), 0)
+        dont_instruction =  inpt[0:search_upto_index+1].rfind("don't()")
+        if do_instruction > dont_instruction:
+            print("DO STUFF")
+        else:
+            continue
+
+        match = match.group(0)
+
+        match = match.replace("mul(","").replace(")","").split(",")
+        match = [int(x) for x in match]
+        running_total += match[0] * match[1]
+
+    return running_total
+
+print(f'Part1 - example: {part1(inpt)}\nPart2 - example: {part2(inpt)}')
--- 2024/day13/day13.py
+++ 2024/day13/day13.py
@@ -1,16 +1,25 @@
 import random, math
 
-def load():
-	with open('day13/day13Input.txt') as file:
-		data = file.readlines()
-		d = [line.strip() for line in data]
-	return d
+def load(example=False):
+    if example is True:
+        with open('day01Example.txt') as file:
+            data = file.readlines()
+            d = [line.strip() for line in data]
+        return d
+        
+    with open('day01Input.txt') as file:
+        data = file.readlines()
+        d = [line.strip() for line in data]
+    return d
 
-inpt = load()
+inpt = load(example=True)
+
 def part1(inpt):
 	return inpt
 
 def part2(inpt):
 	return 0
 
+print(f'Part1 - example: {part1(inpt)}\nPart2 - example: {part2(inpt)}')
+inpt = load(example=False)
 print(f'Part1: {part1(inpt)}\nPart2: {part2(inpt)}')
